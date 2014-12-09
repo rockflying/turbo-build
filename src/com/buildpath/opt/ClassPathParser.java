9027d8034759d0656b9ed3bdebead451e3e9936f
xuhao <rockflyme@gmail.com>
2014-12-09 22:43:30 +0800

    update

--- src/com/buildpath/opt/ClassPathParser.java
+++ src/com/buildpath/opt/ClassPathParser.java
@@ -1,5 +1,6 @@
 package com.buildpath.opt;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -14,31 +15,63 @@ public class ClassPathParser {
 	private static final String ENTRY_PATH     = "path";
 	private static final String ENTRY_EXPORTED = "exported";
 	
-	private List<String> libs;
+	private List<String> jars = new ArrayList<String>();
 
-	public void parse() {
+	public void extractJars(String classpath) {
+		
 		SAXBuilder builder = new SAXBuilder();
+		
+		List<String> projs = new ArrayList<String>();
 
 		try {
-			Document doc = builder.build(buildPath);
+			Document doc = builder.build(classpath);
 			List<Element> list = doc.getRootElement().getChildren();
 
 			for (Iterator<Element> iter = list.iterator(); iter.hasNext();) {
 				Element node = iter.next();
-				System.out.println(node.getAttributeValue(ENTRY_KIND));
-				System.out.println(node.getAttributeValue(ENTRY_PATH));
-				System.out.println(node.getAttributeValue(ENTRY_EXPORTED));
+				String kind = node.getAttributeValue(ENTRY_KIND);
+				String path = node.getAttributeValue(ENTRY_PATH);
+//				String expt = node.getAttributeValue(ENTRY_EXPORTED);
+				if(kind.equals("output")) {
+					System.out.println("Skip output");
+					continue;
+				} else if (kind.equals("src") && path.equals("src")) {
+					System.out.println("Skip src in current project");
+					continue;
+				} else if(kind.equals("con")) {
+					System.out.println("Skip con");
+					continue;
+				} else if (path.startsWith("/")) {
+					projs.add(path);
+				} else {
+					jars.add(path);
+				}
 			}
 
 		} catch (Exception ex) {
 
 			ex.printStackTrace();
 		}
+		
+		getExtraJars(projs);
 	}
 
+	public List<String> getJars() {
+		return jars;
+	}
+
+	private void getExtraJars(List<String> projs){
+		for(Iterator<String> iter = projs.iterator(); iter.hasNext();) {
+			String proj = iter.next();
+			System.out.println(proj);
+		}
+	}
 	public static void main(String[] args) {
 
 		ClassPathParser parser = new ClassPathParser();
-		parser.parse();
+		parser.extractJars("../StaticAnalysis/.classpath");
+		System.out.println(parser.getJars());
+//		String workspace = System.getProperty("user.dir")+"/../";
+
 	}
 }
